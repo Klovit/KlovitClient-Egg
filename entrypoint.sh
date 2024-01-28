f9fb341b8b6820414cfcb47c278db5ad2e69f439
BeastGamer81 <faizpatel81528@gmail.com>
2024-01-28 11:34:45 +0530

    V0.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,3 @@
 #! /bin/sh
 
-cd /home/container || exit 1;
-
-if [[ -f "/home/container/index.js" ]]; then
-  node index.js
-else
-  echo "index.js not found. Proceed to install KlovitClient? (Y/N)";
-  read proceed;
-
-  case "$proceed" in
-    "Y"|"y")
-        echo "Installing KlovitClient...";
-
-        git clone https://github.com/Klovit/KlovitClient.git /home/container/KlovitClient
-        mv /home/container/KlovitClient/** /home/container
-        rm -rf /home/container/KlovitClient
-
-        npm i > /dev/null 2>&1
-        echo "KlovitClient is now downloaded. Please configure settings.json Manually";
-        exit 0;
-        ;;
-    "N"|"n")
-        exit 0;
-        ;;
-    *)
-        exit 1;
-        ;;
-    esac
-fi
+exec env /install.sh
